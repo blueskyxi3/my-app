a36d7940a960ff73b7541653d9beec366e47c2ec
blueskyxi3 <343854933@qq.com>
2024-07-10 15:30:44 +0800

    first commit

---
+++ pkg/test/pages/index.vue
@@ -0,0 +1,11 @@
+<script>
+export default { name: 'MyCustomPage' };
+</script>
+
+<template>
+    <div>
+        <h1>This is a custom VueJS page writed by vincentzou. You can render anything you want</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
